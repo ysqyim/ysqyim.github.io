f2b740ea957247caab7981a28272b2a7088df02a
Hye Soo <kimh711@newschool.edu>
2018-11-06 20:22:07 -0500

    Created files with different name
    API js, html, css folder

---
+++ script.js
@@ -0,0 +1,37 @@
+const searchButton = document.getElementById('searchButton');
+const searchWord = document.getElementById('searchInput');
+
+searchButton.addEventListener("click", getGiphy);
+
+
+function getGiphy() {
+
+	
+    fetch("http://api.giphy.com/v1/gifs/search?q=" + searchWord.value  + "&api_key=1YOfE5g6JayGlLiiyR3EoIWra4MSNMTm&limit=30")
+	
+    .then((giphy) => giphy.json())
+	
+    .then(function(newGiphy) {
+
+        console.log(newGiphy);
+		
+
+	    const resultContainer = document.getElementById('resultContainer');
+        
+        resultContainer.innerHTML = "";
+
+        for (i = 0; i < newGiphy.data.length; i++)  {
+
+	    	const imageUrl = newGiphy.data[i].images.original.url;
+
+	    	const imgElement = document.createElement('img');
+
+	    	imgElement.setAttribute('src', imageUrl);
+
+            imgElement.setAttribute('class', 'giphyImage');
+
+            resultContainer.appendChild(imgElement);
+	    	
+	    }
+	  }).catch(err => null);
+	}
